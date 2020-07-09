9d7c35f323b0a4d6889e8db31fbc8500536356f7
jinho6225 <jinho6225@hotmail.com>
2020-07-08 19:25:53 -0700

    completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,26 +1,47 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    var newTail = Node(value);
+    if (!list.head) {
+      list.head = newTail;
+    }
+
+    if (list.tail) {
+      list.tail.next = newTail;
+    }
+    list.tail = newTail;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    let item = list.head
+    if (list.head.next) {
+      list.head = list.head.next  
+    } else {
+      list.head = null
+    }
+    return item.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    while (list.head) {
+      if (list.head.value === target) {
+        return true
+      }
+      list.head = list.head.next
+    }
+    return false
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
