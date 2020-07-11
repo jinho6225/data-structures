983cc87a0bfdf6c809417f1fb306aa3746a9a820
jinho6225 <jinho6225@hotmail.com>
2020-07-11 09:33:29 -0700

    hashTable insert, retrieve completed

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,26 +1,24 @@
-
-
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  console.log(this._storage)
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
