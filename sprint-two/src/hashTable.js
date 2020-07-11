2317d641cdb2ee7b47bb334278003874f1ab3a5e
jinho6225 <jinho6225@hotmail.com>
2020-07-11 11:57:10 -0700

    insert, retrieve completed with bucket and tuples

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,26 +1,33 @@
 var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.size = 0;
 };
 
 HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (!this._storage[index]) {
+    this._storage[index] = [];
+    this.size++;
+    this._storage[index].push([k, v]);
+  } else {
+    this._storage[index].push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  if (this._storage[index]) {
+    for (let i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        return this._storage[index][i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  function helper(value, idx, arr) {
-    if (index === idx) {
-      arr[idx] = undefined;
-    }
-  }
-  this._storage.each(helper);
 };
 
 /*
